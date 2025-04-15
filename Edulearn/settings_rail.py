88e2f0eef60b86d5a38f35eda1b5c76dbb6b2ea5
Humairafnan <humairaafu716@gmail.com>
2025-04-15 23:10:01 +0600

    sql

--- Edulearn/settings_rail.py
+++ Edulearn/settings_rail.py
@@ -4,3 +4,14 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = ['web-production-52b0.up.railway.app']
+DATABASES={
+    'default':{
+        'Engine': 'django.db.backendspostgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
+        'OPTIONS': {'sslmode' : 'require'},
+    }
+}
