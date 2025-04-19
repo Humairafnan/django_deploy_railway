532ae2558b0305372885e26a30924fd5c2e2c3b4
Humairafnan <humairaafu716@gmail.com>
2025-04-19 19:27:44 +0600

    static file added

--- Edulearn/settings.py
+++ Edulearn/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'courses.apps.CoursesConfig',
     'rest_framework',
+    'whitenoise.runserver_nostatic',
     
 ]
 
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Edulearn.urls'
